c41c67833500f16cccf879854bb0f13835516adc
Jakub Janusz Nawrot <it@jakubnawrot.com>
2023-03-05 16:11:09 +0100

    Files reorganization.

--- src/secrets.h
+++
@@ -1,4 +0,0 @@
-#define _SSID_           "your_wifi_ssid"       // WiFi SSID
-#define _PASSWORD_       "your_wifi_password"   // WiFi Password
-#define _WEBHOOKS_KEY_   "your_webhooks_key"    // Webhooks Key
-#define _WEBHOOKS_EVENT_ "yout_webhooks_event"  // Webhooks Event Name
