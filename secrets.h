0d36231aa7a34532713d08870a46fff053a9c187
Jakub Janusz Nawrot <it@jakubnawrot.com>
2023-07-25 19:11:01 +0200

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -4,3 +4,4 @@
 #define _WEBHOOKS_EVENT_ "yout_webhooks_event"  // Webhooks Event Name
 #define _WEBHOOKS_EVENT_ "your_webhooks_event"  // Webhooks Event Name
 #define _OTA_PASSWORD_   "your_ota_password"    // OTA Password
+test
